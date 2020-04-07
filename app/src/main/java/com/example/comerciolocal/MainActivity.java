41f7447c0a06aaef3162a44c890b32b2c87eae6e
Marcello Albuquerque <marcello.albuca@gmail.com>
2020-04-07 12:15:06 -0300

    estrutura do projeto

---
+++ app/src/main/java/com/example/comerciolocal/MainActivity.java
@@ -0,0 +1,20 @@
+package com.example.comerciolocal;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+
+
+
+import android.os.Bundle;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+
+}
