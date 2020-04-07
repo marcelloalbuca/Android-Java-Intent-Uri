a19457883951372e5ec3f53e71a83121072e51b7
Marcello Albuquerque <marcello.albuca@gmail.com>
2020-04-07 12:35:02 -0300

    função para Intent

--- app/src/main/java/com/example/comerciolocal/MainActivity.java
+++ app/src/main/java/com/example/comerciolocal/MainActivity.java
@@ -20,6 +20,13 @@ public class MainActivity extends AppCompatActivity {
         uri = Uri.parse("http://comerciolocal.camaragibe.pe.gov.br/");
         intent = new Intent(Intent.ACTION_VIEW, uri);
         dispararIntent(intent);
+    }
 
+    private void dispararIntent(Intent intent){
+        if (intent.resolveActivity(getPackageManager()) != null){
+            startActivity(intent);
+        }else{
+            Toast.makeText(this, R.string.erro_intent, Toast.LENGTH_LONG).show();
+        }
     }
 }
